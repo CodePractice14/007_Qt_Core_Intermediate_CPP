8c061ebff411ae93a4d246b5389453d3974d5a37
raizen991 <33425942+GAlexandru14@users.noreply.github.com>
2020-05-19 20:05:13 +0300

    Add files via upload

---
+++ qtci01-1_Parent Child Relationships/main.cpp
@@ -0,0 +1,18 @@
+#include <QCoreApplication>
+#include "test.h"
+
+//01-1. Parent Child Relationships
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    test *p = new test(&a); // should have a parent &a
+
+    p->setObjectName("parent");
+
+    for(int i = 0; i < 5; i++) {
+        p->makeChild(QString::number(i));
+    }
+
+    return a.exec();
+}
